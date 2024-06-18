b568a7aefb420d6797a06edc86fbdf80941a5559
Guru Prasad K N <guruprasadkn29206@gmail.com>
2024-06-18 22:33:37 +0530

    Adding DBConnect Class file

---
+++ DataAccess/DBConnect.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ListOfItems.DataAccess
+{
+    public class DBConnect:DbContext
+    {
+        public DBConnect(DbContextOptions options):base(options) { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
